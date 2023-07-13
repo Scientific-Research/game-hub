40f6eeca787a75e6c1ba2e254dd235005ddb3b11
Maximilian <scientificresearch309@gmail.com>
2023-07-13 22:20:21 +0200

    at this point, we see the name of Game Console on the Browser!

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
 
 interface IProps {
 	game: Game;
@@ -11,6 +11,9 @@ const GameCard = ({ game }: IProps) => {
 			<Image src={game.background_image} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
+				{game.parent_platforms.map(({ platform }) => (
+					<Text>{platform.name}</Text>
+				))}
 			</CardBody>
 		</Card>
 	);
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,13 +2,20 @@ import { useState, useEffect } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+interface Platform {
+	id: number;
+	name: string;
+	slug: string;
+}
+
 export interface Game {
 	id: number;
 	name: string;
 	background_image: string;
+	parent_platforms: { platform: Platform }[]; 
 }
 
-interface FetchGamesResponse {
+interface FetchGamesResponse { 
 	count: number;
 	results: Game[];
 }
