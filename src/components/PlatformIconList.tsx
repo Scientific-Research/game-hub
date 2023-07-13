26bb8891a1aac486e505432ab9322a9204444ade
Maximilian <scientificresearch309@gmail.com>
2023-07-13 23:26:02 +0200

    Displaying Icons related to every item!

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import {
+	FaWindows,
+	FaPlaystation,
+	FaXbox,
+	FaApple,
+	FaLinux,
+	FaAndroid,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
 import { Platform } from '../hooks/useGames';
-import { Text } from '@chakra-ui/react';
+import { HStack, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
 
 interface Props {
 	platforms: Platform[];
 }
 
 export const PlatformIconList = ({ platforms }: Props) => {
+	const iconMap: { [key: string]: IconType } = {
+		// name:PlayStation
+		//slug:playstation
+		pc: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		linux: FaLinux,
+		android: FaAndroid,
+		ios: MdPhoneIphone,
+		web: BsGlobe,
+	};
 	return (
-		<>
+		<HStack>
 			{platforms.map((platform) => (
-				<Text>{platform.name}</Text>
+				// <Text>{platform.name}</Text>
+				<Icon as={iconMap[platform.slug]} />
 			))}
-		</>
+		</HStack>
 	);
 };
