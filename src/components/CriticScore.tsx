cdf2ebd70b96790ab57dd6ffdd6d78e7fa31f65c
Maximilian <scientificresearch309@gmail.com>
2023-07-14 10:55:16 +0200

    adding border,color and finally finished!

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -4,8 +4,14 @@ interface Props {
 	score: number;
 }
 export const CriticScore = ({ score }: Props) => {
+	let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
 	return (
-		<Badge fontSize="14px" paddingX={2}>
+		<Badge
+			colorScheme={color}
+			fontSize="14px"
+			paddingX={2}
+			borderRadius="4px"
+		>
 			{score}
 		</Badge>
 	);
