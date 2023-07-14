27ae45c25e072a4eb41b6f7fef4a5bebf735080d
Maximilian <scientificresearch309@gmail.com>
2023-07-14 10:34:05 +0200

    both of them are in a horizontal line with some space in between!

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,7 @@ const GameCard = ({ game }: IProps) => {
 			<Image src={game.background_image} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
-				<HStack>
+				<HStack justifyContent="space-between">
 					<PlatformIconList
 						platforms={game.parent_platforms.map((p) => p.platform)}
 					/>
