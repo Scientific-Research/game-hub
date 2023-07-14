5298eaf152bd47190d30c0f55ba45da16a6f7dac
Maximilian <scientificresearch309@gmail.com>
2023-07-14 10:31:31 +0200

    now, both of them are in a horizontal line!

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
 import { PlatformIconList } from './PlatformIconList';
 import { CriticScore } from './CriticScore';
 
@@ -13,10 +13,12 @@ const GameCard = ({ game }: IProps) => {
 			<Image src={game.background_image} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
-				<PlatformIconList
-					platforms={game.parent_platforms.map((p) => p.platform)}
-				/>
-				<CriticScore score={game.metacritic} />
+				<HStack>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
 			</CardBody>
 		</Card>
 	);
