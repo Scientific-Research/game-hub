a736ae012987cfdcb4b4c17048142f6975445f47
Maximilian <scientificresearch309@gmail.com>
2023-07-15 17:00:56 +0200

    changing the text to button as link!

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,12 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import {
+	Button,
+	HStack,
+	Image,
+	List,
+	ListItem,
+	Spinner,
+	Text,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
@@ -18,7 +26,9 @@ export const GenreList = () => {
 							borderRadius={8}
 							src={getCroppedImageUrl(genre.image_background)}
 						/>
-						<Text fontSize="lg">{genre.name}</Text>
+						<Button fontSize="lg" variant="link">
+							{genre.name}
+						</Button>
 					</HStack>
 				</ListItem>
 			))}
