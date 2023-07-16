bf8b8e9d75cd74bbb23c12164e59192da723988a
Maximilian <scientificresearch309@gmail.com>
2023-07-16 18:18:44 +0200

    Order by works in Console.log() till now!

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,8 +1,15 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 
-
 export const SortSelector = () => {
+	const sortOrders = [
+		{ value: '', label: 'Relevance' },
+		{ value: '-added', label: 'Date added' },
+		{ value: 'name', label: 'Name' },
+		{ value: '-released', label: 'Release date' },
+		{ value: '-metacritic', label: 'Popularity' },
+		{ value: '-rating', label: 'Average rating' },
+	];
 	return (
 		<Menu>
 			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
@@ -10,12 +17,21 @@ export const SortSelector = () => {
 				Order by: Relevance
 			</MenuButton>
 			<MenuList>
-				<MenuItem>Relevance</MenuItem>
+				{sortOrders.map((order) => (
+					<MenuItem
+						onClick={() => console.log(order.label)}
+						key={order.value}
+						value={order.value}
+					>
+						{order.label}
+					</MenuItem>
+				))}
+				{/* <MenuItem>Relevance</MenuItem>
 				<MenuItem>Date added</MenuItem>
 				<MenuItem>Name</MenuItem>
 				<MenuItem>Release date</MenuItem>
 				<MenuItem>popularity</MenuItem>
-				<MenuItem>Average rating</MenuItem>
+				<MenuItem>Average rating</MenuItem> */}
 				{/* {data.map((platform) => (
 					<MenuItem
 						onClick={() => onSelectPlatform(platform)}
