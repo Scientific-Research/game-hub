97a53b2d9213a41d77a1b0defb829f715b0ef13f
Maximilian <scientificresearch309@gmail.com>
2023-07-17 11:17:28 +0200

    Handles game with no image!

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,12 @@
+import noImage from '../assets/No-Image-Placeholder.svg.png';
+
 const getCroppedImageUrl = (url: string) => {
 	if (url !== null) {
 		const target = 'media/';
 		const index = url.indexOf(target) + target.length;
 		return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+	} else {
+		return noImage;
 	}
 };
 
